18b54beff48c8809e31490437dd2af8267471a68
Rachs <shadrachgabriel@icloud.com>
2018-08-21 14:25:54 +0800

    mekaniko ni monico

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,5 @@
 import Model.FSM;
+import Model.Makina;
 import Model.Node;
 
 import java.util.ArrayList;
@@ -6,15 +7,15 @@ import java.util.ArrayList;
 public class Driver {
     public static void main(String[] args) {
     //test
-       ArrayList<Node> b = new FSM().getFsm(); //initializes fsm
-        for (int i = 0; i < b.size(); i++) {
-            System.out.println(b.get(i).getsType() + " State: " + b.get(i).getState() + "\n");
-            System.out.println("Links: ");
-            for (int j = 0; j < b.get(i).getpLinks().size(); j++)
-                System.out.println(b.get(i).getpLinks().get(j).getState());
-            System.out.println("-------------------");
-        }
-
+//       ArrayList<Node> b = new FSM().getFsm(); //initializes fsm
+//        for (int i = 0; i < b.size(); i++) {
+//            System.out.println(b.get(i).getsType() + " State: " + b.get(i).getState() + "\n");
+//            System.out.println("Links: ");
+//            for (int j = 0; j < b.get(i).getpLinks().size(); j++)
+//                System.out.println(b.get(i).getpLinks().get(j).getState());
+//            System.out.println("-------------------");
+//        }
+    new Makina(0, 21);
     }
 
 
