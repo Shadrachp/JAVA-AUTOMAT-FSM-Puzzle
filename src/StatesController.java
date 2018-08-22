3c7f1ef204443ba45fe6a5c6b11f830f50c32c7a
prbg <plinkygamara@gmail.com>
2018-08-22 19:10:50 +0800

    highlight states

--- src/StatesController.java
+++ src/StatesController.java
@@ -24,7 +24,28 @@ public class StatesController {
         switch(string) {
             case "00000": setColor(label0, isValid); break;
             case "00110": setColor(label1, isValid); break;
+            case "00010": setColor(label2, isValid); break;
+            case "10110": setColor(label3, isValid); break;
+            case "00100": setColor(label4, isValid); break;
+            case "01110": setColor(label5, isValid); break;
+            case "00111": setColor(label6, isValid); break;
+            case "00001": setColor(label7, isValid); break;
+            case "01000": setColor(label8, isValid); break;
+            case "111010": setColor(label9, isValid); break;
+            case "01111": setColor(label10, isValid); break;
+            case "11110": setColor(label11, isValid); break;
+            case "11000": setColor(label12, isValid); break;
+            case "10111": setColor(label13, isValid); break;
+            case "00101": setColor(label14, isValid); break;
+            case "10000": setColor(label15, isValid); break;
+            case "11011": setColor(label16, isValid); break;
+            case "10001": setColor(label17, isValid); break;
+            case "11001": setColor(label18, isValid); break;
+            case "11101": setColor(label19, isValid); break;
+            case "01001": setColor(label20, isValid); break;
 
+            case "11111": setColor(label21, isValid); break;
+            default: setColor(dead, isValid);
         }
     }
 
@@ -39,7 +60,8 @@ public class StatesController {
 
     public void resetStates() {
         reset();
-        labels[0].setStyle("-fx-background-color: yellow; -fx-background-radius: 50%");
+        label0.setStyle("-fx-background-color: yellow; -fx-background-radius: 50%");
+        label21.setStyle("-fx-background-color: green; -fx-background-radius: 50%");
     }
 
     public void reset() {
