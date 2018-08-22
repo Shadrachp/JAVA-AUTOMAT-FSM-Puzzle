d62c9e66e5031261fe35133acae07d9c1894eea6
prbg <plinkygamara@gmail.com>
2018-08-22 17:06:00 +0800

    deleted Makina.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,3 @@
-import Model.Makina;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -24,16 +23,6 @@ public class Driver extends Application {
     }
 
     public static void main(String[] args) {
-    //test
-//       ArrayList<Node> b = new FSM().getFsm(); //initializes fsm
-//        for (int i = 0; i < b.size(); i++) {
-//            System.out.println(b.get(i).getsType() + " State: " + b.get(i).getState() + "\n");
-//            System.out.println("Links: ");
-//            for (int j = 0; j < b.get(i).getpLinks().size(); j++)
-//                System.out.println(b.get(i).getpLinks().get(j).getState());
-//            System.out.println("-------------------");
-//        }
-    new Makina(0, 21);
         launch(args);
     }
 
